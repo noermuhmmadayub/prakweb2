3da605d5177651a0abe55caa330aa1d9731500a1
Noer Muhammad Ayub <noermuhmmadayub@gmail.com>
2023-05-26 15:52:09 +0700

    belajar template laravel

--- olshop/app/Http/Controllers/ForminputController.php
+++ olshop/app/Http/Controllers/ForminputController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class ForminputController extends Controller
 {
-    //
+    public function index(){
+        return view ('form');
+    }
+    public function form(Request $request){
+        $data = [
+            'nama' => $request->input('nama'),
+            'email' => $request->input('email'),
+            'lokasi' => $request->input('lokasi'),
+            'jk' => $request->input('jk'),
+            'skill' => $request->input('skill'),
+
+        ];
+        return view('form', compact('data'));
+    }
 }
--- olshop/routes/web.php
+++ olshop/routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InputController;
+use App\Http\Controllers\ForminputController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProdukController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -40,3 +45,18 @@ Route::get('/pemeriksa', function(){
 
 Route::get('/input', [InputController::class, 'index']);
 Route::post('/output',[InputController::class, 'output']); 
+
+Route::get('/form', [ForminputController::class, 'index']);
+Route::post('/form', [ForminputController::class, 'form']);
+
+// ini route tampilan admin
+Route::prefix('admin')->group(function (){
+Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/produk', [ProdukController::class, 'index']);
+});
+
+// route untuk tampilan frontend
+Route::prefix('frontend')->group(function (){
+Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/about', [AboutController::class, 'index']);
+});
